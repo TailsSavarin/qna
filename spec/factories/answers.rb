1285bfd0f2e162f7f8ef32b6fca26de2542af724
TailsSavarin <Alexql7@gmail.com>
2020-06-05 17:22:01 +0300

    Add answer controller & tests for his actions

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :answer do
-    question { nil }
-    body { 'MyText' }
+    question
+    body { 'AnswerBody' }
+
+    trait :invalid do
+      body { nil }
+    end
   end
 end
