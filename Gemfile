bfe37f365a11703eed238639aafb64ef1f8f0928
TailsSavarin <Alexql7@gmail.com>
2020-06-08 16:35:45 +0300

    User can sign in, story realized

--- Gemfile
+++ Gemfile
@@ -65,6 +65,8 @@ group :test do
   gem 'shoulda-matchers'
   # Tests for controllers
   gem 'rails-controller-testing'
+  # For save_and_open_page option
+  gem 'launchy'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -31,8 +31,9 @@ RSpec.describe AnswersController, type: :controller do
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'saves a new answer in the database' do
-        expect { post :create, params: { question_id: question,
-                                         answer: attributes_for(:answer) } }.to change(question.answers, :count).by(1)
+        expect {
+          post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        }.to change(question.answers, :count).by(1)
       end
 
       it 'redirects to show view' do
@@ -43,8 +44,9 @@ RSpec.describe AnswersController, type: :controller do
 
     context 'with invalid attributes' do
       it 'does not save the question' do
-        expect { post :create, params: { question_id: question,
-                                         answer: attributes_for(:answer, :invalid) } }.to_not change(question.answers, :count)
+        expect {
+          post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        }.to_not change(question.answers, :count)
       end
 
       it 're-renders new view' do
