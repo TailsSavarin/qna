f7b4820d903de6bc36a5c45cfe6ad585fb64e396
TailsSavarin <Alexql7@gmail.com>
2020-06-08 19:40:42 +0300

    User can sign out, story realized

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -6,7 +6,11 @@ RSpec.describe QuestionsController, type: :controller do
 
     before { get :index }
 
-    it 'populates an array of all questions' do
+    it 'assigns all Questions to @questions' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
       expect(response).to render_template :index
     end
   end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 feature 'User can sign in', %q(
   In order to ask questions
-  As an anauthenticated user
+  As an authenticated user
   I'd like to be able to sign in
 ) do
-  given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
+  given(:user) { create(:user) }
 
+  # Boot path
   background { visit new_user_session_path }
 
-  scenario 'Registered user tries to sign in' do
+  scenario 'Registered user with valid data tries to sign in' do
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     click_on 'Log in'
@@ -17,8 +18,8 @@ feature 'User can sign in', %q(
     expect(page).to have_content 'Signed in successfully.'
   end
 
-  scenario 'Unregistered user tries to sign in' do
-    fill_in 'Email', with: 'user@test.com'
+  scenario 'Unregistered or user with invalid data tries to sign in' do
+    fill_in 'Email', with: 'wrong@test.com'
     fill_in 'Password', with: '12345678'
     click_on 'Log in'
     # save_and_open_page
