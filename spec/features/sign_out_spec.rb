9463935c0179334f01df9326ee4a707db531429f
TailsSavarin <Alexql7@gmail.com>
2020-06-08 19:40:42 +0300

    User can sign out, story realized

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q(
+  In order to end the session
+  As an authenticated user
+  I'd like to be able to sign out
+) do
+  given(:user) { create(:user) }
+
+  scenario 'Authenticated user tries to sign out' do
+    sign_in(user)
+
+    click_on 'Log out'
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
