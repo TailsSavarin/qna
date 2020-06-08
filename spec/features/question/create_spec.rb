2cea5d9cdd0b1be4512c8949fc99197aff2a7dd6
TailsSavarin <Alexql7@gmail.com>
2020-06-08 23:42:36 +0300

    User can create a question, story realized with acceptance tests

---
+++ spec/features/question/create_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'User can create question', %q(
+  In order to get answer from a community
+  As an user
+  I'd like to be able to create a question
+) do
+
+  background do
+    visit questions_path
+    click_on 'Create question'
+  end
+
+  scenario 'User create a question' do
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text text text'
+    click_on 'Ask'
+
+    expect(page).to have_content 'Your question successfully created.'
+    expect(page).to have_content 'Test question'
+    expect(page).to have_content 'text text text'
+  end
+
+  scenario 'User create a question with errors' do
+    click_on 'Ask'
+    # save_and_open_page
+    expect(page).to have_content "Title can't be blank"
+  end
+end
