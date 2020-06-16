c4a2f2037a2d5deb40fc768699c67739a40976b8
TailsSavarin <Alexql7@gmail.com>
2020-06-16 11:02:31 +0300

    Create active storage & realized story where authenticated user can add files when creating a question

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -28,6 +28,16 @@ feature 'User can create question', %q(
       expect(page).to have_content "Title can't be blank"
       expect(page).to have_content "Body can't be blank"
     end
+
+    scenario 'create a question with attached files' do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text text'
+      attach_file 'File', [Rails.root / 'spec' / 'rails_helper.rb', Rails.root / 'spec' / 'spec_helper.rb']
+      click_on 'Create question'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
   end
 
   scenario 'unauthenticated user tries to ask a question' do
