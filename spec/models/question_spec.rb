393365b555aa4b3d50c5121daa8737abfaf35b16
TailsSavarin <Alexql7@gmail.com>
2020-06-19 13:29:58 +0300

    User can offer several links to the question when creating and add cocoon-js

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,15 +3,18 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   describe 'associations' do
     it { should belong_to(:user) }
+    it { should have_many(:links).dependent(:destroy) }
     it { should have_many(:answers).dependent(:destroy) }
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
+    it { should validate_presence_of(:title) }
   end
 
   it 'have many attached files' do
     expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
   end
+
+  it { should accept_nested_attributes_for(:links) }
 end
