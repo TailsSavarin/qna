8032822d7caa6c645f63883b2a3116ddb28bc265
TailsSavarin <Alexql7@gmail.com>
2020-06-21 17:08:13 +0300

    Refactoring question links, reward views and tests for this objects

--- spec/features/reward/index_spec.rb
+++ spec/features/reward/index_spec.rb
@@ -6,22 +6,23 @@ feature 'User can watch his rewards', %q(
   I'd like to be able to watch watch my rewards
 ) do
   given(:user) { create(:user) }
-  given(:question) { create(:question, user: user) }
-  given(:rewards) { create_list(:reward, 5, user: user) }
-
+  given!(:reward) { create(:reward, user: user) }
+  
   scenario 'authenticated user view his rewards' do
+    reward.image.attach(create_file_blob(filename: 'test.jpg'))
     sign_in(user)
-
-    visit question_path(question)
-
-    click_on 'My rewards'
+    visit rewards_path
 
     expect(page).to have_content 'Your Rewards List'
+
+    expect(page).to have_link reward.question.title
+    expect(page).to have_content reward.title
+    have_css("img[src*='test.jpg']")
   end
 
   scenario 'unauthenticated user can not view rewards' do
-    visit question_path(question)
+    visit questions_path
 
-    expect(page).to_not have_link 'My rewards'
+    expect(page).to_not have_link 'My Rewards'
   end
 end
