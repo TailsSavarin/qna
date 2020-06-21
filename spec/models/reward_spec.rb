a90f1ba8ebefff3c5434fbf2a2316c77acea8999
TailsSavarin <Alexql7@gmail.com>
2020-06-21 23:18:14 +0300

    Refactoring views and fixing models tests

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe Reward, type: :model do
     it { should belong_to(:user).optional }
   end
 
-  describe 'validations' do
-    it { should validate_presence_of(:title) }
-  end
-
   it 'have one image' do
     expect(Reward.new.image).to be_an_instance_of(ActiveStorage::Attached::One)
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
   end
 end
