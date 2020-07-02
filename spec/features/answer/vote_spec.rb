d3b0a89e58640db1d7b2738e6410eaf5da5fa3bb
TailsSavarin <Alexql7@gmail.com>
2020-07-02 19:29:32 +0300

    Authenticated user cannot vote for his question/answer & fixing tests

--- spec/features/answer/vote_spec.rb
+++ spec/features/answer/vote_spec.rb
@@ -7,7 +7,8 @@ feature 'Authenticated user can vote for the answer that he liked', %q(
 ) do
   given(:user) { create(:user) }
   given(:question) { create(:question) }
-  given!(:answer) { create(:answer, question: question) }
+  given!(:answer) { create(:answer, question: question, user: user) }
+  given!(:another_answer) { create(:answer, question: question) }
 
   describe 'authenticated user', js: true do
     background do
@@ -15,34 +16,47 @@ feature 'Authenticated user can vote for the answer that he liked', %q(
       visit question_path(question)
     end
 
-    scenario 'votes up for the answer he like' do
-      within '.answers' do
-        expect(page).to have_link 'Up'
-        click_on 'Up'
-
-        expect(page).to have_content '1'
-        expect(page).to_not have_link 'Up'
+    context 'author of the answer' do
+      scenario 'cannot vote for his anwer' do
+        within "#answer-#{answer.id}" do
+          expect(page).to_not have_link 'Up'
+          expect(page).to_not have_link 'Down'
+        end
       end
     end
 
-    scenario 'votes down against the answer' do
-      within '.answers' do
-        expect(page).to have_link 'Down'
-        click_on 'Down'
+    context 'not the author of the answer' do
+      scenario 'votes up for the answer he like' do
+        within "#answer-#{another_answer.id}" do
+          expect(page).to have_link 'Up'
+          click_on 'Up'
 
-        expect(page).to have_content '-1'
-        expect(page).to_not have_link 'Down'
+          expect(page).to have_content '1'
+          expect(page).to_not have_link 'Up'
+        end
       end
-    end
 
-    scenario 'revote the answer' do
-      within '.answers' do
-        expect(page).to have_link 'Revote'
-        click_on 'Revote'
+      scenario 'votes down against the answer' do
+        within "#answer-#{another_answer.id}" do
+          expect(page).to have_link 'Down'
+          click_on 'Down'
 
-        expect(page).to have_link 'Up'
-        expect(page).to have_link 'Down'
-        expect(page).to_not have_link 'Revote'
+          expect(page).to have_content '-1'
+          expect(page).to_not have_link 'Down'
+        end
+      end
+
+      scenario 'revote the answer' do
+        within "#answer-#{another_answer.id}" do
+          expect(page).to_not have_link 'Revote'
+          click_on 'Up'
+          expect(page).to have_link 'Revote'
+          click_on 'Revote'
+
+          expect(page).to have_link 'Up'
+          expect(page).to have_link 'Down'
+          expect(page).to_not have_link 'Revote'
+        end
       end
     end
   end
