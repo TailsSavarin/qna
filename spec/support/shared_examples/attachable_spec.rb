14c70b3f4815b7b5f28dfb618930f943da423110
TailsSavarin <Alexql7@gmail.com>
2020-07-17 01:09:30 +0300

    New devise sign up form & fixing features test

--- spec/support/shared_examples/attachable_spec.rb
+++ spec/support/shared_examples/attachable_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 shared_examples_for 'attachable' do
+  let(:model) { described_class }
+
   describe 'associations' do
     it 'have many attached files' do
-      expect(Answer.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+      expect(model.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
     end
   end
 end
