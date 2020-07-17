bcaec47b267ee43edd9739a2e1a9a3bafba0a922
TailsSavarin <Alexql7@gmail.com>
2020-07-18 00:02:20 +0300

    Add sessions folder

--- spec/support/shared_examples/attachable.rb
+++ spec/support/shared_examples/attachable.rb
@@ -1,9 +1,7 @@
 shared_examples_for 'attachable' do
-  let(:model) { described_class }
-
   describe 'associations' do
     it 'have many attached files' do
-      expect(model.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+      expect(described_class.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
     end
   end
 end
