cdec151149d85693d505c7a39f81f3af2479ab80
TailsSavarin <Alexql7@gmail.com>
2020-07-23 19:58:41 +0300

    Add question list #GET /api/v1/questions

---
+++ spec/api/v1/questions_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+describe 'Questions API', type: :request do
+  let(:headers) { { 'ACCEPT' => 'application/json' } }
+
+  describe 'GET /api/v1/questions' do
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get '/api/v1/questions', headers: headers
+
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access_token is invalid' do
+        get '/api/v1/questions', params: { access_token: '1234' }, headers: headers
+
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let(:question) { questions.first }
+      let(:question_json) { json.first }
+      let(:access_token) { create(:access_token) }
+      let!(:questions) { create_list(:question, 2) }
+      let!(:answers) { create_list(:answer, 2, question: question) }
+
+      before { get '/api/v1/questions', params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'returns list of questions' do
+        expect(json.size).to eq 2
+      end
+
+      it 'returns all publick fields' do
+        %w[id title body user_id created_at updated_at].each do |attr|
+          expect(question_json[attr]).to eq question.send(attr).as_json
+        end
+      end
+
+      describe 'answers' do
+        let(:answer) { answers.first }
+        let(:answer_json) { question_json['answers'].first }
+
+        it 'return list of answers' do
+          expect(question_json['answers'].size).to eq 2
+        end
+
+        it 'returns all public fields' do
+          %w[id body user_id created_at updated_at].each do |attr|
+            expect(answer_json[attr]).to eq answer.send(attr).as_json
+          end
+        end
+      end
+    end
+  end
+end
