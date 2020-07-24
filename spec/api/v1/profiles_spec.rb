172c7a467a7b59bb2be3939909f7f7840eaea7a8
TailsSavarin <Alexql7@gmail.com>
2020-07-24 17:58:12 +0300

    Add profile list #GET /api/v1/profiles

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -4,23 +4,24 @@ describe 'Profiles API', type: :request do
   let(:headers) { { 'ACCEPT' => 'application/json' } }
 
   describe 'GET /api/v1/profiles/me' do
+    let(:api_path) { '/api/v1/profiles/me' }
+
     it_behaves_like 'API Authorizable' do
       let(:method) { :get }
-      let(:api_path) { '/api/v1/profiles/me' }
     end
 
     context 'authorized' do
       let(:me) { create(:user) }
       let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
-      before { get '/api/v1/profiles/me', params: { access_token: access_token.token }, headers: headers }
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
 
-      it 'returns 200 status' do
+      it 'returns success status' do
         expect(response).to be_successful
       end
 
       it 'returns all public fields' do
-        %w[id email admin created_at updated_at].each do |attr|
+        %w[id email created_at updated_at admin].each do |attr|
           expect(json[attr]).to eq me.send(attr).as_json
         end
       end
@@ -28,6 +29,40 @@ describe 'Profiles API', type: :request do
       it 'does not return private fields' do
         %w[password encrypted_password].each do |attr|
           expect(json).to_not have_key(attr)
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/profiles' do
+    let(:api_path) { '/api/v1/profiles' }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let(:me) { create(:user) }
+      let(:user) { users.first }
+      let!(:users) { create_list(:user, 2) }
+      let(:user_json) { json['users'].first }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns success status' do
+        expect(response).to be_successful
+      end
+
+      it 'returns all public fields' do
+        %w[id email created_at updated_at admin].each do |attr|
+          expect(user_json[attr]).to eq user.send(attr).as_json
+        end
+      end
+
+      it 'does not return private fields' do
+        %w[password encrypted_password].each do |attr|
+          expect(user_json).to_not have_key(attr)
         end
       end
     end
