72a0dd4e87e2f2c048028a77827846624b8e271d
TailsSavarin <Alexql7@gmail.com>
2020-07-24 19:23:01 +0300

    Add separate answer #GET /api/v1/answers/:id

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::AnswersController < Api::V1::BaseController
     @question = Question.find(params[:question_id])
     render json: @question.answers, each_serializer: AnswersSerializer
   end
+
+  def show
+    @answer = Answer.with_attached_files.find(params[:id])
+    render json: @answer, serializer: AnswerSerializer
+  end
 end
