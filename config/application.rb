d77febf0117e82d4ca2e69847ba8c798d2616620
TailsSavarin <Alexql7@gmail.com>
2020-07-28 18:14:43 +0300

    Add sidekiq Gem

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module Qna
     # Directory loading
     config.autoload_paths += [config.root.join('app')]
 
+    # Sidekiq
+    confi.active_job.queue_adapter = :sidekiq
+
     # Rspec options for rails g controller
     config.generators do |g|
       g.test_framework :rspec,
