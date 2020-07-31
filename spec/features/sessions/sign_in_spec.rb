e036ddae4863ce09c6279efc5d189e006f34c460
TailsSavarin <Alexql7@gmail.com>
2020-08-01 02:00:41 +0300

    Refactoring features sessions specs

--- spec/features/sessions/sign_in_spec.rb
+++ spec/features/sessions/sign_in_spec.rb
@@ -7,94 +7,71 @@ feature 'User can sign in', %q(
 ) do
   given(:confirmed_user) { create(:user) }
   given(:unconfirmed_user) { create(:user, confirmed_at: '') }
+
   background { visit new_user_session_path }
 
-  describe 'registered user tries to sign in' do
+  context 'registered user signs in' do
     scenario 'with confirmed email' do
       within '.card-body' do
         fill_in 'Email', with: confirmed_user.email
         fill_in 'Password', with: confirmed_user.password
-
         click_on 'Log in'
       end
 
       expect(page).to have_content 'Signed in successfully.'
+      expect(current_path).to eq root_path
     end
 
     scenario 'with unconfirmed email' do
       within '.card-body' do
         fill_in 'Email', with: unconfirmed_user.email
         fill_in 'Password', with: unconfirmed_user.password
-
         click_on 'Log in'
       end
 
       expect(page).to have_content 'You have to confirm your email address before continuing.'
+      expect(current_path).to eq new_user_session_path
     end
   end
 
-  scenario 'unregistered or with invalid data, user tries to sign in' do
+  scenario 'non-registered user try to sign in' do
     within '.card-body' do
       fill_in 'Email', with: 'unregistered@test.com'
       fill_in 'Password', with: '12345678'
-
       click_on 'Log in'
     end
 
     expect(page).to have_content 'Invalid Email or password.'
+    expect(current_path).to eq new_user_session_path
   end
 
-  describe 'sign in with GitHub' do
-    scenario 'user tries to sign in' do
-      mock_auth_github
-
-      click_on 'Sign in with GitHub'
-
-      expect(page).to have_content 'Successfully authenticated from Github account.'
-    end
-
-    scenario 'user handle authentication error' do
-      OmniAuth.config.mock_auth[:github] = :invalid_credentials
-
-      click_on 'Sign in with GitHub'
-
-      expect(page).to have_content %(Could not authenticate you from GitHub because "Invalid credentials")
+  context 'sign in with GitHub' do
+    it_behaves_like 'sign in features' do
+      given(:mock_auth) { mock_auth_github }
+      given(:social_network) { 'GitHub' }
+      given(:mock_auth_error) do
+        OmniAuth.config.mock_auth[:github] = :invalid_credentials
+      end
     end
   end
 
-  describe 'sign in with Twitter' do
-    scenario 'user tries to sign in' do
-      mock_auth_twitter
-
-      click_on 'Sign in with Twitter'
-
-      expect(page).to have_content 'Successfully authenticated from Twitter account.'
-    end
-
-    scenario 'user handle authentication error' do
-      OmniAuth.config.mock_auth[:twitter] = :invalid_credentials
-
-      click_on 'Sign in with Twitter'
-
-      expect(page).to have_content %(Could not authenticate you from Twitter because "Invalid credentials")
+  context 'sign in with Twitter' do
+    it_behaves_like 'sign in features' do
+      given(:mock_auth) { mock_auth_twitter }
+      given(:social_network) { 'Twitter' }
+      given(:mock_auth_error) do
+        OmniAuth.config.mock_auth[:twitter] = :invalid_credentials
+      end
     end
   end
 
-  describe 'sign in with Facebook' do
-    scenario 'user tries to sign in' do
-      mock_auth_facebook
-
-      click_on 'Sign in with Facebook'
-
-      expect(page).to have_content 'Successfully authenticated from Facebook account.'
-    end
-
-    scenario 'user handle authentication error' do
-      OmniAuth.config.mock_auth[:facebook] = :invalid_credentials
-
-      click_on 'Sign in with Facebook'
-
-      expect(page).to have_content %(Could not authenticate you from Facebook because "Invalid credentials")
+  context 'sign in with Facebook' do
+    it_behaves_like 'sign in features' do
+      given(:mock_auth) { mock_auth_facebook }
+      given(:social_network) { 'Facebook' }
+      given(:mock_auth_error) do
+        OmniAuth.config.mock_auth[:facebook] = :invalid_credentials
+      end
     end
   end
 end
---
+++ spec/support/shared/features/sign_in_social_examples.rb
@@ -0,0 +1,15 @@
+shared_examples 'sign in features' do
+  scenario 'try to sign in' do
+    mock_auth
+    click_on "Sign in with #{social_network}"
+
+    expect(page).to have_content "Successfully authenticated from #{social_network.capitalize} account."
+  end
+
+  scenario 'handle authentication error' do
+    mock_auth_error
+    click_on "Sign in with #{social_network}"
+
+    expect(page).to have_content %(Could not authenticate you from #{social_network} because "Invalid credentials")
+  end
+end
