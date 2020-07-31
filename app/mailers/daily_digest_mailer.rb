a4ef25e152755061f4870bf1feda00be2e180b82
TailsSavarin <Alexql7@gmail.com>
2020-07-31 18:17:21 +0300

    Small correction

--- app/mailers/daily_digest_mailer.rb
+++ app/mailers/daily_digest_mailer.rb
@@ -1,9 +1,4 @@
 class DailyDigestMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.daily_digest_mailer.digest.subject
-  #
   def digest(user)
     @questions = Question.where(created_at: 24.hours.ago..Time.current)
 
