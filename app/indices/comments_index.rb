b59c704ed00456c928e563fed6c5aae32d9146d7
TailsSavarin <Alexql7@gmail.com>
2020-08-03 22:14:01 +0300

    Add indices for modles and sphinx settings

---
+++ app/indices/comments_index.rb
@@ -0,0 +1,7 @@
+ThinkingSphinx::Index.define :comment, with: :active_record do
+  # fields
+  indexes body
+  indexes user.email, as: :user, sortable: true
+
+  # attributes
+  has commentable_id, commentable_type, user_id, created_at, updated_at
