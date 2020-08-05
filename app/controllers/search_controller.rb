9d33c5810408abfcd78d402dd0c68ed1cb95b2e3
TailsSavarin <Alexql7@gmail.com>
2020-08-05 03:25:31 +0300

    Add pages for search fix W.P. Gem for bootstrap4

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,6 @@ class SearchController < ApplicationController
   def index
     @resource = params[:resource]
     @query = params[:query]
-    @results = SearchService.new(@resource, @query).call
+    @results = SearchService.new(@resource, @query).call.paginate(page: params[:page], per_page: 5)
   end
 end
