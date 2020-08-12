0d30297bcb686ef62fac08243ca029a4d798c4bb
TailsSavarin <Alexql7@gmail.com>
2020-08-12 12:34:03 +0300

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'gmail.com'
+    domain: 'gmail.com',
     user_name: Rails.application.credentials[Rails.env.to_sym][:mailer][:user_name],
     password: Rails.application.credentials[Rails.env.to_sym][:mailer][:password],
     authentication: 'plain',
