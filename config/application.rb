15b34adffe0d9f7bd8b615000e83ba1895fc383e
TailsSavarin <Alexql7@gmail.com>
2020-08-17 13:48:36 +0300

    Add redis-rails Gem

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Qna
     # Sidekiq
     config.active_job.queue_adapter = :sidekiq
 
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
+
     # Rspec options for rails g controller
     config.generators do |g|
       g.test_framework :rspec,
