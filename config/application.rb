30c69334eb1dba0f5a569431f6bd204ce4b8dc7b
TailsSavarin <Alexql7@gmail.com>
2020-08-17 14:38:34 +0300

    Add caching for questions answers and comments

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,6 @@ module Qna
     # Sidekiq
     config.active_job.queue_adapter = :sidekiq
 
-    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
-
     # Rspec options for rails g controller
     config.generators do |g|
       g.test_framework :rspec,
